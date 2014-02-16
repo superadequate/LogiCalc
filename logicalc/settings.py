b323a5e659683992c10634aa78e0df7ae5dea265
pwhipp <paul.whipp@gmail.com>
2014-02-17 09:23:47 +1000

    Updated settings for postgres database and local configuration

--- logicalc/settings.py
+++ logicalc/settings.py
@@ -94,7 +94,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -103,7 +103,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = 'US/Eastern'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
@@ -344,7 +344,7 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 # ignored in your version control system allowing for settings to be
 # defined per machine.
 try:
-    from local_settings import *
+    from logicalc.local_settings import *
 except ImportError:
     pass
 
