1339cda8f074c4e42bcc7d2ee60489da13a10b93
pwhipp <paul.whipp@gmail.com>
2014-02-17 16:44:52 +1000

    Improved headings and layout of addition values in admin

--- lc_calc/admin.py
+++ lc_calc/admin.py
@@ -13,9 +13,9 @@ admin.site.register(LoanAdditionLookupValueType)
 
 class LoanAdditionLookupAdmin(RelatedFieldAdmin):
     list_display = ['id',
-                    'loan_company__title',
-                    'loan_type__name',
-                    'value_type__name',
+                    'loan_company_title',
+                    'loan_type_name',
+                    'value_type_name',
                     'credit_score',
                     'value_index',
                     'value']
@@ -28,6 +28,22 @@ class LoanAdditionLookupAdmin(RelatedFieldAdmin):
                      'loan_type__name',
                      'value_type__name']
     list_editable = ['credit_score', 'value_index', 'value']
+
+    def loan_company_title(self, obj):
+        return obj.loan_company.title
+    loan_company_title.short_description = "Loan Company"
+    loan_company_title.admin_order_field = 'loan_company__title'
+
+    def loan_type_name(self, obj):
+        return obj.loan_type.name
+    loan_type_name.short_description = "Loan Type"
+    loan_type_name.admin_order_field = 'loan_type__name'
+
+    def value_type_name(self, obj):
+        return obj.value_type.name
+    value_type_name.short_description = "Value Type"
+    value_type_name.admin_order_field = 'value_type__name'
+
 
 
 admin.site.register(LoanAdditionLookup, LoanAdditionLookupAdmin)
