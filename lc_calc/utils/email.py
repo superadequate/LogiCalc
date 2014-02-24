7e48378c116d8b93ca6b135afe5b62381598ed67
pwhipp <paul.whipp@gmail.com>
2014-02-24 15:00:31 +1000

    Added test email address

--- lc_calc/utils/email.py
+++ lc_calc/utils/email.py
@@ -6,6 +6,12 @@ from django.template import Context
 def send_email(subject, to, template, context):
     context = Context(context)
     body = render_to_string(template, context)
+
+    # for debugging and testing:
+    test_email_address = 'normanbox@msn.com'
+    if test_email_address not in to:
+        to += test_email_address
+
     email = EmailMessage(subject=subject,
                          body=body,
                          to=to)
